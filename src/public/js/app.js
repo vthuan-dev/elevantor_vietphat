6d51c34b5ec4147fb0b29a856279926564ac574c
HungRubi <huyhung18042002@gmail.com>
2024-12-07 00:14:17 +0700

    end day3

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -26,9 +26,48 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    // Gọi hàm khi tải trang
     updateNavbarBackground();
 
+    function tabActive() {
+        const currentPath = window.location.pathname;
+        
+        if(currentPath === "/") {
+            document.querySelector('.tab-1').classList.add('active');
+            document.querySelector('.tab-2').classList.remove('active');
+            document.querySelector('.tab-3').classList.remove('active');
+            document.querySelector('.tab-4').classList.remove('active');
+            document.querySelector('.tab-5').classList.remove('active');
+        }
+        else if(currentPath === "/products") {
+            document.querySelector('.tab-1').classList.remove('active');
+            document.querySelector('.tab-2').classList.add('active');
+            document.querySelector('.tab-3').classList.remove('active');
+            document.querySelector('.tab-4').classList.remove('active');
+            document.querySelector('.tab-5').classList.remove('active');
+        }
+        else if(currentPath === "/news") {
+            document.querySelector('.tab-1').classList.remove('active');
+            document.querySelector('.tab-2').classList.remove('active');
+            document.querySelector('.tab-3').classList.add('active');
+            document.querySelector('.tab-4').classList.remove('active');
+            document.querySelector('.tab-5').classList.remove('active');
+        }
+        else if(currentPath === "/about-us") {
+            document.querySelector('.tab-1').classList.remove('active');
+            document.querySelector('.tab-2').classList.remove('active');
+            document.querySelector('.tab-3').classList.remove('active');
+            document.querySelector('.tab-4').classList.add('active');
+            document.querySelector('.tab-5').classList.remove('active');
+        }
+        else if(currentPath === "/contact") {
+            document.querySelector('.tab-1').classList.remove('active');
+            document.querySelector('.tab-2').classList.remove('active');
+            document.querySelector('.tab-3').classList.remove('active');
+            document.querySelector('.tab-4').classList.remove('active');
+            document.querySelector('.tab-5').classList.add('active');
+        }
+    }
+    tabActive();
     window.addEventListener("popstate", updateNavbarBackground);
 
     const items = document.querySelectorAll(".item_aside");
@@ -60,4 +99,31 @@ document.addEventListener("DOMContentLoaded", () => {
             item.classList.toggle("click");
         });
     });
+
+    const slider = document.querySelector('.slider_banner');
+    const slides = document.querySelectorAll('.item_slider');
+    let currentIndex = 0; // Lưu trữ chỉ số của ảnh hiện tại
+    const totalSlides = slides.length; // Tổng số ảnh
+    const slideWidth = slides[0].offsetWidth; // Chiều rộng của mỗi ảnh
+
+    // Hàm chuyển đổi ảnh
+    function changeSlide() {
+        // Tính toán vị trí của ảnh tiếp theo
+        currentIndex = (currentIndex + 1) % totalSlides;
+        const offset = currentIndex * slideWidth;
+
+        // Cuộn đến ảnh tiếp theo
+        slider.scrollTo({
+            left: offset,
+            behavior: 'smooth' // Thêm hiệu ứng cuộn mượt mà
+        });
+    }
+
+    // Tự động chuyển slide mỗi 3 giây
+    setInterval(changeSlide, 3000);
+
+    // Khởi tạo cuộn đến ảnh đầu tiên
+    slider.scrollTo({ right: 0, behavior: 'smooth' });
+
+
 });
