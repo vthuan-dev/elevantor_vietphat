00420ad0c94e20b33abf3db162d33c9ccf086d23
HungRubi <huyhung180420022gmail.com>
2025-01-10 18:49:16 +0700

    day7

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,26 +1,14 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    const tabCard = document.querySelectorAll('.list_card_container');
-    const btnTabCard = document.querySelectorAll('.tab_item');
-
-    let indexSate = 0;
-
-    btnTabCard.forEach((btn, index) => {
-        let tab = tabCard[index];
-        btn.addEventListener('click', () => {
-            document.querySelector('.list_card_container.active').classList.remove('active');
-            document.querySelector('.tab_item.active').classList.remove('active')
-
-            btn.classList.add('active')
-            tab.classList.add('active');
-            indexSate = index;
-            getProductForSategory();
-        })
-    })
-    async function getProductForSategory() {
+    function starts(){
+        getProductForCop();
+        getProductForDien();
+        getProductForInox();
+    }
+    starts();
+    async function getProductForCop() {
         const sategoryProduct = await fetch(`http://localhost:4000/products/api/getcop`);
         const product = await sategoryProduct.json();
-        console.log(product);
         
         const copList = [
             document.querySelector('.cop-list-1'),
@@ -56,7 +44,101 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     
     }
-    getProductForSategory();
+    async function getProductForDien() {
+        const sategoryProduct = await fetch(`http://localhost:4000/products/api/getdien`);
+        const product = await sategoryProduct.json();
+        
+        const dienList = [
+            document.querySelector('.dien-list-1'),
+            document.querySelector('.dien-list-2'),
+        ];
+        let dienChuck = [];
+        for (let i = 0; i < product.length; i += 4) {
+            dienChuck.push(product.slice(i, i + 4));
+        }
+        dienChuck.forEach(function (chuck, index) {
+            let htmls = chuck.map(function (product) {
+                    return `
+                    <li class="item_card">
+                        <a href="/products/${product.slug}" class="wrapper_img">
+                            <img class="fade-in" src=${product.thumbnail_main} alt="">
+                            <i class="bi bi-plus"></i>
+                        </a>
+                        <div class="title_product">
+                            <h3>
+                                <a href="/products/${product.slug}">
+                                    ${product.name}
+                                </a>
+                            </h3>
+                        </div>
+                    </li>`;
+                })
+                .join('');
+            if (dienList[index]) {
+                dienList[index].innerHTML = htmls;
+            }
+            const newImages = document.querySelectorAll('.fade-in');
+            newImages.forEach(img => observer.observe(img));
+        });
+    
+    }
+    async function getProductForInox() {
+        const sategoryProduct = await fetch(`http://localhost:4000/products/api/getinox`);
+        const product = await sategoryProduct.json();
+        const inoxList = [
+            document.querySelector('.inox-list-1'),
+            document.querySelector('.inox-list-2'),
+        ];
+        let inoxChuck = [];
+        for (let i = 0; i < product.length; i += 4) {
+            inoxChuck.push(product.slice(i, i + 4));
+        }
+        inoxChuck.forEach(function (chuck, index) {
+            let htmls = chuck.map(function (product) {
+                    return `
+                    <li class="item_card">
+                        <a href="/products/${product.slug}" class="wrapper_img">
+                            <img class="fade-in" src=${product.thumbnail_main} alt="">
+                            <i class="bi bi-plus"></i>
+                        </a>
+                        <div class="title_product">
+                            <h3>
+                                <a href="/products/${product.slug}">
+                                    ${product.name}
+                                </a>
+                            </h3>
+                        </div>
+                    </li>`;
+                })
+                .join('');
+            if (inoxList[index]) {
+                inoxList[index].innerHTML = htmls;
+            }
+            const newImages = document.querySelectorAll('.fade-in');
+            newImages.forEach(img => observer.observe(img));
+        });
+    
+    }
+    const tabCard = document.querySelectorAll('.list_card_container');
+    const btnTabCard = document.querySelectorAll('.tab_item');
+
+    let indexSate = 0;
+
+    btnTabCard.forEach((btn, index) => {
+        let tab = tabCard[index];
+        btn.addEventListener('click', () => {
+            document.querySelector('.list_card_container.active').classList.remove('active');
+            document.querySelector('.tab_item.active').classList.remove('active')
+
+            btn.classList.add('active')
+            tab.classList.add('active');
+            indexSate = index;
+            getProductForSategory();
+        })
+    })
+    
+
+
     const imgRight = document.querySelectorAll('.animation-right');
     const imgTop = document.querySelectorAll('.animation-top');
     const fadeIn = document.querySelectorAll('.fade-in');
