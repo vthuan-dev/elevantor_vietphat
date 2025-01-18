c02debfbedb376752c3c7f8ef081b6d060fa5466
vthuan.dev <165972755+vthuan-dev@users.noreply.github.com>
2025-01-18 23:42:43 +0700

    fix2

--- src/public/config/config.js
+++ src/public/config/config.js
@@ -2,5 +2,5 @@ window.env = {
   YOUR_USER_ID: "R3fDVNALwN2P-wC-D",
   YOUR_SERVICE_ID: "service_1cf4qwy",
   YOUR_TEMPLATE_ID: "template_hixexyh",
-  SERVER: "http://localhost:4000",
+  SERVER: "https://admin-elevator-vietphat.onrender.com",
 };
